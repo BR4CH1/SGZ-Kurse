55b2e3d228c6fbb5ee7561a9e5a1fd0fd5e92974
BR4CH1 <schraderm@web.de>
2026-08-26 13:27:21 +0200

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/SGZ-Kurse/',
   plugins: [react()],
   server: {
     host: true,
